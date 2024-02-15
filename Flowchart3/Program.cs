2fad2f5839ba83d2398805017ae842b805cc9828
NahidM8 <nahidbm@code.edu.az>
2024-02-15 14:09:17 +0400

    Task4

---
+++ Flowchart3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp=Convert.ToInt32(Console.ReadLine());
+            if (temp == 15)
+            {
+                Console.WriteLine("15 derecedir");
+            }
+            else if (temp>15)
+            {
+                Console.WriteLine("Hava istidir");
+            }
+            else
+            {
+                Console.WriteLine("Hava soyuqdur");
+            }
+        }
+    }
