08a6c47627874cac9941e64a7db417873f28c2be
NahidM8 <nahidbm@code.edu.az>
2024-02-15 13:31:12 +0400

    Task2

---
+++ Flowchart1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 63;
+            if (n % 21 == 0)
+            {
+                Console.WriteLine("bolunur");
+            }
+            else
+            {
+                Console.WriteLine("bolunmur");
+            }
+
+        }
+    }
