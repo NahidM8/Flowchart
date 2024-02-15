92180bb71010f65e8e27471638a837acc2742f8d
NahidM8 <nahidbm@code.edu.az>
2024-02-15 13:46:35 +0400

    Task3

---
+++ Flowchart2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=Convert.ToInt32(Console.ReadLine());
+            int m=Convert.ToInt32(Console.ReadLine());
+            if(n%2==0 && m%2==0)
+            {
+                Console.WriteLine(n + m);
+            }
+            else
+            {
+                Console.WriteLine("duzgun eded daxil et");
+            }
+        }
+    }
