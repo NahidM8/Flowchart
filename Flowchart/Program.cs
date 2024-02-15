6088a2aa85b3c61a91067e1947522da18fa174d3
NahidM8 <nahidbm@code.edu.az>
2024-02-15 13:11:11 +0400

    Task1

---
+++ Flowchart/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 9;
+            if (n == 0)
+            {
+                Console.WriteLine("Duzgun eded daxil et");
+            }
+            else if (n % 2 == 1)
+            {
+                Console.WriteLine("Tekdir");
+            }
+            else
+            {
+                Console.WriteLine("Cutdur");
+            }
+
+        }
+    }
+
